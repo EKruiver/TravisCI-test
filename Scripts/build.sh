28cfd9b3540265ac422de8576868ebcf12016ddf
EKruiver <krui0034@hz.nl>
2018-10-16 11:48:18 +0200

    test build 11

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -14,12 +14,12 @@ echo "Attempting to build $project for Windows"
 
 echo "Attempting to build $project for OS X"
 /Applications/Unity/Unity.app/Contents/MacOS/Unity 
-  -batchmode \ 
-  -nographics \
-  -silent-crashes \ 
-  -logFile $(pwd)/unity.log \ 
-  -projectPath $(pwd) \
-  -buildOSXUniversalPlayer "$(pwd)/Build/osx/$project.app" \
+  -batchmode 
+  -nographics 
+  -silent-crashes  
+  -logFile $(pwd)/unity.log  
+  -projectPath $(pwd) 
+  -buildOSXUniversalPlayer "$(pwd)/Build/osx/$project.app" 
   -quit
 
 echo "Attempting to build $project for Linux"
